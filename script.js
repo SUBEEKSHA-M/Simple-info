e713deac20bd620902e9e102a3efa394aada3156
SUBEEKSHA-M <subee22it@gmail.com>
2025-01-10 11:34:23 +0530

    mod

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (event) {
-        event.preventDefault(); 
-
-        const sectionId = this.getAttribute('href').substring(1); 
-        const targetSection = document.getElementById(sectionId);
-
-        if (targetSection) {
-            targetSection.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start',
-            });
-        }
-    });
-});
-
-document.querySelectorAll('#skills ul li').forEach(skill => {
-    skill.addEventListener('click', () => {
-        alert(`You clicked on skill: ${skill.textContent}`);
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        const target = document.querySelector(this.getAttribute('href'));
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
     });
 });
